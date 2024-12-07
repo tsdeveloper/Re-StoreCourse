4fdc7244a92cf7ae9bd8e2cbda387e521f184022
Thyago Developer <ts.analista@gmail.com>
2024-12-06 22:18:28 -0300

    feat: add Faker entity seed data.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,29 +1,57 @@
+using API.Data;
 using API.Extensions;
+using API.Seed;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var serviceProvider = builder.Services.BuildServiceProvider();
-var conf = serviceProvider.GetRequiredService<IConfiguration>();
-builder.Services.AddApplicationServices(conf);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
+    Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
 
-app.UseStaticFiles();
-app.UseSerilogRequestLogging();
-app.UseHttpsRedirection();
-app.MapControllers();
-app.UseCors("CorsPolicyAllowFront");
-app.Run();
+    Log.Information("Starting up");
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddControllers();
+    builder.Services.AddSerilog();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var serviceProvider = builder.Services.BuildServiceProvider();
+    var conf = serviceProvider.GetRequiredService<IConfiguration>();
+    builder.Services.AddApplicationServices(conf);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseStaticFiles();
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.UseCors("CorsPolicyAllowFront");
+
+    using var scope = serviceProvider.CreateScope();
+    var context = serviceProvider.GetRequiredService<RestoreCourseDbContext>();
+    await context.Database.MigrateAsync();
+
+    if (true)
+        await RestoreCourseContextSeed.SeedAsync(context);
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "App down");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
