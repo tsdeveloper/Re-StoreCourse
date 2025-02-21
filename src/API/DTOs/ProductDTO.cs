72de88b29c592a6916ef8d17836dd3b373bdeb8f
Thyago Developer <ts.analista@gmail.com>
2025-02-20 21:06:18 -0300

    feat: resolve bug mapper return data wrong.

--- src/API/DTOs/ProductDTO.cs
+++ src/API/DTOs/ProductDTO.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities.Products;
 
 namespace API.DTOs;
 
@@ -10,11 +11,11 @@ public class ProductReturnDTO
     public int Id { get; set; }
     public int TypeId { get; set; }
     public int BrandId { get; set; }
+    public ProductBrandReturnDTO Brand { get; set; }
+    public ProductTypeReturnDTO Type { get; set; }
+    public int QuantityInStock { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public long Price { get; set; }
     public string PictureUrl { get; set; }
-    public ProductTypeReturnDTO Type { get; set; }
-    public ProductBrandReturnDTO Brand { get; set; }
-    public int QuantityInStock { get; set; }
 }
