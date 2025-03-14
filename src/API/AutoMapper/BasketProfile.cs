dbe4a2937e0dfb1279c2d1f3218ffb4858a714b1
Thyago Developer <ts.analista@gmail.com>
2025-03-13 21:25:42 -0300

    feat: add mapping member domain to DTO basket item

--- src/API/AutoMapper/BasketProfile.cs
+++ src/API/AutoMapper/BasketProfile.cs
@@ -10,6 +10,11 @@ public class BasketProfile : Profile
   public BasketProfile()
   {
     CreateMap<Basket, BasketReturnDTO>();
-    CreateMap<BasketItem, BasketItemReturnDto>();
+    CreateMap<BasketItem, BasketItemReturnDto>()
+      .ForMember(d => d.Name, o => o.MapFrom(s => s.Product.Name))
+      .ForMember(d => d.Price, o => o.MapFrom(s => s.Product.Price))
+      .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl))
+      .ForMember(d => d.Quantity, o => o.MapFrom(s => s.Quantity))
+      ;
   }
 }
