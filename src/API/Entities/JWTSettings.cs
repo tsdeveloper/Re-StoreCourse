58b6e997246ec81091ff3d832b99005daf8347bc
Thyago Developer <ts.analista@gmail.com>
2025-08-11 23:33:34 -0300

    feat: add JWT token service. Add Options Pattern JWT Settings.

---
+++ src/API/Entities/JWTSettings.cs
@@ -0,0 +1,10 @@
+namespace API.Entities;
+
+public class JWTSettings
+{
+    public string TokenKey { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public double Expire { get; set; }
+    public double ExpireRefreshToken { get; set; }
+}
