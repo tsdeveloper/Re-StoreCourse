4a80486911e72b696e8ad723105d942c6efa1272
Thyago Developer <ts.analista@gmail.com>
2025-09-11 11:18:03 -0300

    feat: add PaymentIntendId to Basket and Order tables for payment processing

---
+++ src/API/Migrations/20250828015715_fieldPaymentIntendIdTableOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class fieldPaymentIntendIdTableOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntendId",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntendId",
+                table: "Order");
+        }
+    }
+}
